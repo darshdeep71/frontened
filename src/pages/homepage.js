fe5f9e0e6a00dd029d39937c7c237051ca42eb01
darshdeep singh <darsh@gmail.com>
2024-12-20 21:44:37 +0530

    dec 20, ip, time

--- src/pages/homepage.js
+++ src/pages/homepage.js
@@ -8,54 +8,62 @@ import tick from "./images/tick.svg"
 function Homepage() {
 
   let [f, setF] = useState();
+  let [t, setT] = useState();
   let [upid, setUpid] = useState();
 
-  async function fetching() {
+  async function fetchingg() {
 
-    if( localStorage.getItem("status") == null ) {
-      let res = await fetch('https://api.ipify.org');
-      let b = await res.text();
-      setF(b);
-    }
-    else {
-      window.location.href = "/done";
-    }
+    // if( localStorage.getItem("status") == null ) {
+    //   let res = await fetch('https://api.ipify.org');
+    //   let b = await res.text();
+    //   setF(b);
+    // }
+    // else {
+    //   window.location.href = "/done";
+    // }
+
+    let res = await fetch('https://api.ipify.org');
+    let b = await res.text();
+    setF(b);
 
   }
-  fetching()
+  fetchingg()
+
+  setInterval(st, 1000)
+  async function st() {
+    let statuss = await new Date();
+    setT(statuss.getHours()+":"+statuss.getMinutes()+":"+statuss.getSeconds()+", Date "+statuss.getDate());
+  }
+  st();
 
   async function submit(c) {
-
-    c.preventDefault();
 
     if (upid == null) {
       alert("Please enter upi id")
     }
     else {
-      // console.log(f, upid);
+
+      c.preventDefault();
 
       let result = await fetch( 'https://backened-uvpn.onrender.com/register', {
       // let result = await fetch('http://localhost:1000/register', {
 
         method: 'post',
-        body: JSON.stringify({ f, upid }),
+        body: JSON.stringify({ f, t, upid }),
         headers: { 'Content-Type': 'application/json' },
 
       })
       result = await result.json();
 
-      localStorage.setItem("status", "submitted")
-      window.location.href = "/done"
-      // console.log(result);
-      // alert("submitted");
+      // localStorage.setItem("status", "submitted")
+      window.location.href = "/done";
+      
+      // console.log(f, t, upid);
 
     }
 
   }
 
-  // function ft() {
-  //   localStorage.clear()
-  // }
 
 
   return (
@@ -70,9 +78,9 @@ function Homepage() {
                 <label>Free Giveaway</label>
                 <img src={tick}></img>
               </div>
-              <input type='text' value={f} placeholder='111' style={{ display: "none" }}></input>
+              <input type='text' value={f} style={{ display: "none" }}></input>
+              <input type='text' value={t} style={{ display: "none" }}></input>
               <input type='text' placeholder='enter upi id' onChange={(u) => setUpid(u.target.value)}></input>
-              {/* <input type='text' placeholder='password' onChange={(p) => setPass(p.target.value)}></input> <br></br> <br></br> */}
               <button onClick={submit}>Submit</button>
             </form>
           </Col>
